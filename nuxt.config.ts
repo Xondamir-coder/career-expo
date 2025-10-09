c86f90b6fb8e58152262999a1fc60e51ec32510e
Xondamir <xondamirnazrullayev@gmail.com>
2025-10-09 19:09:40 +0500

    nuxt upgrade (3->4), google tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: '2024-11-01',
-	devtools: { enabled: true },
-
-	vite: {
-		css: {
-			preprocessorOptions: {
-				scss: {
-					additionalData:
-						'@use "~/assets/styles/abstracts/_variables.scss" as *; @use "~/assets/styles/abstracts/_mixins.scss" as *;'
-				}
-			}
-		}
-	},
-	app: {
-		head: {
-			titleTemplate: '%s - Career Expo',
-			link: [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }]
-		}
-	},
-	css: ['~/assets/styles/main.scss'],
-	modules: ['nuxt-swiper', '@nuxtjs/seo']
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: false },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+					  @use "~/assets/styles/abstracts/_variables.scss" as *; 
+						@use "~/assets/styles/abstracts/_mixins.scss" as *;`
+        }
+      }
+    }
+  },
+  app: {
+    head: {
+      titleTemplate: '%s - Career Expo',
+      link: [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
+      script: [
+        {
+          src: 'https://www.googletagmanager.com/gtag/js?id=G-G2DGZWQ3ZJ',
+          async: true
+        },
+        {
+          innerHTML: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-G2DGZWQ3ZJ');
+          `,
+          type: 'text/javascript'
+        }
+      ]
+    }
+  },
+  css: ['~/assets/styles/main.scss'],
+  modules: ['nuxt-swiper', '@nuxtjs/seo']
 });
